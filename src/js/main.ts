f70db7944dd49df07a113551f6e43ef654510ee5
im-sad <im-sad@ya.ru>
2026-06-12 17:20:48 +0300

    lazy img load detection

--- src/js/main.ts
+++ src/js/main.ts
@@ -2,11 +2,13 @@ import { setScrollWidth } from '@utils/scroll-width'
 import { iosFixes } from '@utils/ios-fixes'
 import { initModals } from '@modules/init-modals'
 import { initTips } from '@modules/init-tip'
+import { initLazyImg } from '@modules/init-lazy-img'
 
 // DOM loaded
 document.addEventListener('DOMContentLoaded', () => {
   setScrollWidth()
   iosFixes()
+  initLazyImg()
 })
 
 // All resources loaded
