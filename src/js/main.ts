365648cce7e226b1491539360774cef60907fb1e
im-sad <im-sad@ya.ru>
2026-06-28 17:01:23 +0300

    include root fz module

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,3 +1,4 @@
+import { rootFz } from '@utils/root-fz'
 import { setScrollWidth } from '@utils/scroll-width'
 import { iosFixes } from '@utils/ios-fixes'
 import { initModals } from '@modules/init-modals'
@@ -6,6 +7,7 @@ import { initLazyImg } from '@modules/init-lazy-img'
 
 // DOM loaded
 document.addEventListener('DOMContentLoaded', () => {
+  rootFz()
   setScrollWidth()
   iosFixes()
   initLazyImg()
