dca41636a8cebff085018510a767c02c4e39eb61
im-sad <im-sad@ya.ru>
2026-07-08 12:44:57 +0300

    calc widgets heights

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,4 +1,5 @@
 import { rootFz } from '@utils/root-fz'
+import { setWidgetsHeight } from '@utils/widgets-height'
 import { setScrollWidth } from '@utils/scroll-width'
 import { iosFixes } from '@utils/ios-fixes'
 import { initModals } from '@modules/init-modals'
