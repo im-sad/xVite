0a708196ea9bed819605550f20a2552e20ff2bc4
im-sad <im-sad@ya.ru>
2026-07-08 14:28:06 +0300

    tips back

--- src/js/main.ts
+++ src/js/main.ts
@@ -3,7 +3,7 @@ import { setWidgetsHeight } from '@utils/widgets-height'
 import { setScrollWidth } from '@utils/scroll-width'
 import { iosFixes } from '@utils/ios-fixes'
 import { initModals } from '@modules/init-modals'
-import { initDialog } from '@modules/init-dialog'
+import { initTips } from '@modules/init-tip'
 import { initLazyImg } from '@modules/init-lazy-img'
 
 // DOM loaded
