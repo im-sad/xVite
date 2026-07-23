9f3458da40825c5b2a7897fd6499e65a713fe73b
im-sad <im-sad@ya.ru>
2026-07-23 14:16:42 +0300

    fix modal aria-hidden

--- src/js/modules/init-modals.ts
+++ src/js/modules/init-modals.ts
@@ -4,9 +4,11 @@ export const initModals = (): HystModal => {
   const modals = new HystModal({
     linkAttributeName: 'data-open-modal',
     waitTransitions: true,
-    beforeOpen: (modalInstance: HystModalInstance) => {
+    beforeOpen: (_modalInstance: HystModalInstance) => {},
+    afterClose: (modalInstance: HystModalInstance) => {
       const target: HTMLElement = modalInstance.element
-      console.log(target)
+
+      target.setAttribute('aria-hidden', 'true')
     }
   })
 
